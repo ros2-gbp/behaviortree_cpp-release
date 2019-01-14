d6d3ece8e98ca3fbea9e83b6cfce4868025c11ff
Davide Facont <davide.faconti@gmail.com>
2019-01-14 22:06:29 +0100

    Rebase from 'release/melodic/behaviortree_cpp'

--- include/behaviortree_cpp/xml_parsing.h
+++ include/behaviortree_cpp/xml_parsing.h
@@ -19,10 +19,7 @@ class XMLParser
 
     void loadFromText(const std::string& xml_text);
 
-    using NodeBuilder = std::function<TreeNode::Ptr(const std::string&, const std::string&,
-                                                    const NodeParameters&, TreeNode::Ptr)>;
-
-    TreeNode::Ptr instantiateTree(std::vector<TreeNode::Ptr>& nodes);
+    TreeNode::Ptr instantiateTree(std::vector<TreeNode::Ptr>& nodes, const Blackboard::Ptr &blackboard);
 
   private:
 
